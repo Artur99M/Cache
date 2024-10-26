6df656d4c1cd66c1922546ae9c4036aca95da540
Artur <minnakhmetov.aa@phystech.edu>
2024-10-26 13:36:41 +0300

    In perfect_cache.sh there is mistake

--- test/perfect_cache.sh
+++ test/perfect_cache.sh
@@ -2,7 +2,7 @@
 cat $2 | while read size push_number ref_output output_PC input
 do
     echo "$size $push_number $input" >> $1/test/tstp.txt
-    $1/perfect_cache 0< $1/test/tstp.txt > $1/test/testp.txt
+    $1/build/PC.out > $1/test/testp.txt 0< $1/test/tstp.txt
     # echo "number = $number, size = $size, push_number = $push_number, input = $input, ref_output = $ref_output, real_output = $real_output"
     read output 0< $1/test/testp.txt
     echo "PERFECT CACHE TEST return $output"
