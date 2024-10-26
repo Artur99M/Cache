5785836a89f3305eba29f0ed83741bf636e3b5d6
Artur <minnakhmetov.aa@phystech.edu>
2024-10-26 12:49:07 +0300

    Cmake was made

--- test/perfect_cache.sh
+++ test/perfect_cache.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-cat $1 | while read size push_number ref_output input
+cat $2 | while read size push_number ref_output output_PC input
 do
-    echo "$size $push_number $input" >> test/tstp.txt
-    ./perfect_cache 0< test/tstp.txt > test/testp.txt
+    echo "$size $push_number $input" >> $1/test/tstp.txt
+    $1/perfect_cache 0< $1/test/tstp.txt > $1/test/testp.txt
     # echo "number = $number, size = $size, push_number = $push_number, input = $input, ref_output = $ref_output, real_output = $real_output"
-    read output 0< test/testp.txt
+    read output 0< $1/test/testp.txt
     echo "PERFECT CACHE TEST return $output"
-    rm test/tstp.txt test/testp.txt
+    rm $1/test/tstp.txt $1/test/testp.txt
+    if [[ output -ne output_PC ]]
+    then
+        echo "TEST FAIL: real = $output, ref = $output_PC"
+        exit 1
+    else
+        echo "TEST OK!"
+        exit 0
+    fi
 done
--- test/test.sh
+++ test/test.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-cat $1 | while read size push_number ref_output input
+cat $2 | while read size push_number ref_output output_PC input
 do
-    echo "$size $push_number $input" >> test/tst.txt
-    ./LFU.out 0< test/tst.txt > test/test.txt
-    read real_output 0< test/test.txt
-    rm test/test.txt
+    echo "$size $push_number $input" >> $1/test/tst.txt
+    $1/build/LFU.out 0< $1/test/tst.txt > $1/test/test.txt
+    read real_output 0< $1/test/test.txt
+    rm $1/test/test.txt
     # echo "number = $number, size = $size, push_number = $push_number, input = $input, ref_output = $ref_output, real_output = $real_output"
+    read line
+    rm $1/test/tst.txt
     if [[ ref_output -eq real_output ]]
     then
         echo "TEST OK!"
+        exit 0
     else
         echo "ERROR in test: ref = $ref_output, real = $real_output"
         exit 1
     fi
-    read line
-    rm test/tst.txt
 done
