b719c993fa94945251bea480f7a49cd8bb80233e
Artur <minnakhmetov.aa@phystech.edu>
2024-11-26 21:47:42 +0300

    it's 100 times faster than last comit

--- PC/include/perfect_cache.hpp
+++ PC/include/perfect_cache.hpp
@@ -1,9 +1,10 @@
-#include <iostream>
-#include <vector>
+#pragma once
 #include <unordered_map>
 #include <list>
-#include <tuple>
-
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <iostream>
 #ifdef DEBUG
 static auto & debug = std::cerr;
 #else
@@ -22,149 +23,130 @@ class debuging
 debuging debug;
 }
 #endif
-
 namespace cache
 {
-
-    template <class T> static size_t find(const T * const arr, size_t arr_size, const T elem)
+    template<typename T, typename keyT = int>
+    struct PC_cache
     {
-        for (size_t i = 0; i < arr_size; i++)
-            if (arr[i] == elem) return i;
-
-        return 0xDEAD0BED;
-    }
-
-    template <class T, class keyT = int>
-    size_t lookup_update_perfect (keyT* lookup, size_t n_hits, T (*slow_get_page)(keyT), const size_t cache_size)
-    {
-    struct cache_part
-    {
-        T elem_;
-        keyT key_;
-        size_t next_using;
-
-        static cache_part func(T elem, keyT key, size_t next)
+        private:
+        //TYPES
+        struct TK
         {
-            cache_part p;
-            p.key_ = key;
-            p.next_using = next;
-            p.elem_ = elem;
-            return p;
-        }
-    };
-    debug << "start\n";
-    struct PCache
-    {
-        std::list<cache_part> cache_;
-        using it = typename std::list<cache_part>::iterator;
+            T value_;
+            keyT key_;
+            size_t next_using;
+        };
+        using it = typename std::list<TK>::iterator;
+        using cit = typename std::list<TK>::const_iterator;
+        // using it_h = typename std::unordered_map<keyT, cit>::iterator;
+
+        //MEMBERS
+        std::list<TK> cache_;
         std::unordered_map<keyT, it> hash_;
         std::unordered_map<keyT, std::list<size_t>> lookup_;
+        size_t sz_ = 5;
+
+        cit elem_out () const
+        {
+            cit out = cache_.begin();
+            cit end = cache_.end();
+            for (cit i = out; i != end; ++i)
+            {
+                if (out->next_using < i->next_using)
+                    out = i;
+            }
+
+            return out;
+        }
+        PC_cache () {}
+        public:
+        explicit PC_cache (size_t sz) : sz_(sz) {if (sz_ <= 0) sz_ = 5;}
+        inline size_t size() const {return sz_;}
+        inline size_t set_size (size_t sz)
+        {
+            if (sz > 0)
+            {
+                if (sz < sz_)
+                {
+                    for (int i = sz_; i > sz; i++)
+                    {
+                        auto oit = elem_out();
+                        hash_.erase((*oit).key_); // for this line key_ was added in TK
+                        cache_.erase (oit);
+                    }
+                }
+                sz_ = sz;
+            }
+            return sz_;
+        }
+
+        void make_lookup(keyT* lookup, size_t n)
+        {
+            for (size_t i = 0; i < n; ++i)
+            {
+                auto f = lookup_.find(lookup[i]);
+                if (f == lookup_.end())
+                {
+                    std::list<size_t> l;
+                    l.emplace_front(i);
+                    lookup_.emplace(lookup[i], l);
+                }
+                else
+                {
+                    (f->second).emplace_back(i);
+                }
+            }
+            for (auto i = lookup_.begin(); i != lookup_.end(); ++i)
+            {
+                (i->second).emplace_back(0xDEAD0BED);
+                debug << i->first << ": ";
+                for (auto j = i->second.begin(); j != i->second.end(); j++)
+                    debug << *j << ' ';
+                debug << '\n';
+            }
+
+        }
+
+        template <class F> bool lookup_update (keyT key, F slow_get_page)
+        {
+            debug << "\nkey = " << key << "\ncache:\n";
+            for (it i = cache_.begin(); i != cache_.end(); ++i)
+            {
+                debug << '{' << i->next_using << ", " << i->key_ << "} ";
+            }
+            debug << '\n';
+            auto hit = hash_.find(key);
+            std::list<size_t>& l = lookup_[key];
+            l.erase(l.begin());
+            size_t next_using = *(l.begin());
+            debug << "next_using = " << next_using << '\n';
+
+            if (hit == hash_.end())
+            {
+                debug << "no in cache\n";
+                if (next_using == 0xdead0bed) return false;
+                if (sz_ == cache_.size())
+                {
+                    auto oit = elem_out ();
+                    debug << "oit = {" << oit->next_using << ", " << oit->key_ << "}\n";
+                    debug << "oit->next_using < next_using = " << (oit->next_using < next_using) << '\n';
+                    if (oit->next_using < next_using)
+                        return false;
+                    hash_.erase((*oit).key_); // for this line key_ was added in TK
+                    cache_.erase (oit);
+                }
+                TK new_elem = {slow_get_page (key), key, next_using};
+                cache_.emplace_front (new_elem);
+                hash_.emplace (key, cache_.begin());
+
+                return false;
+            }
+            else
+            {
+                debug << "in cache\n";
+                hit->second->next_using = next_using;
+                return true;
+            }
+        }
     };
-    size_t value = 0;
-
-    PCache pcache;
-
-    for (size_t i = 0; i < n_hits; ++i)
-    {
-        debug << __LINE__ << ": find " << lookup[i] << ", i = " << i << '\n';
-        auto f = pcache.lookup_.find(lookup[i]);
-        if (f == pcache.lookup_.end())
-        {
-            debug << "no in lookup_\n";
-            std::list<size_t> v = {i};
-            f = (pcache.lookup_.emplace (lookup[i], v)).first;
-            debug << "final of \"in\"\n";
-        }
-        else
-        {
-            debug << "in lookup_\n";
-            // std::list<size_t> & v = f->second;
-            (f->second).emplace_back(i);
-        }
-    }
-
-    for (auto i = pcache.lookup_.begin(); i != pcache.lookup_.end(); ++i)
-    {
-        (i->second).emplace_back(0xDEAD0BED);
-    }
-    debug << "end of building pcache.lookup_\n";
-
-    auto out = pcache.cache_.end();
-
-    for (size_t i_hit = 0; i_hit < n_hits; ++i_hit)
-    {
-        keyT hit = lookup[i_hit];
-        debug << i_hit << ":\ncache: ";
-        #ifdef DEBUG
-        for (auto i =  pcache.cache_.begin(); i != pcache.cache_.end(); ++i)
-        {
-            debug << '{' << i->elem_ << ", " << i->next_using << "} ";
-        }
-        #endif
-        debug << "\nhit = " << hit << '\n';
-        debug << "out = {" << out->elem_ << ", " << out->next_using << "}\n";
-        auto&& hhit = pcache.hash_.find(hit);
-
-        auto& v = pcache.lookup_.find(hit)->second;
-        v.erase (v.begin());
-        size_t next_using = *(v.begin());
-        // debug << "next_using = " << next_using << '\n';
-        // // for (size_t i = 0; i < v.size(); ++i)
-        // //     debug << v[i] << ", ";
-        // // debug << '\n';
-
-        if (hhit != pcache.hash_.end())
-        {
-            debug << "in cache\n";
-            ++value;
-            auto& chit = hhit->second;
-            chit->next_using = next_using;
-            if (chit->next_using > out->next_using) out = chit;
-        }
-        else
-        {
-            debug << "no in cache, next_using = ";
-            if (next_using == 0xDEAD0BED)
-            {
-                continue;
-            }
-            debug << next_using << '\n';
-
-            if (pcache.cache_.size() < cache_size)
-            {
-                pcache.cache_.emplace_front(cache_part::func((*slow_get_page)(hit), hit, next_using));
-                pcache.hash_.emplace(hit, pcache.cache_.begin());
-                out = pcache.cache_.begin();
-                for (auto i = pcache.cache_.begin(); i != pcache.cache_.end(); ++i)
-                {
-                    if (out->next_using < i->next_using)
-                        out = i;
-                }
-            }
-            else if (out->next_using > next_using)
-            {
-                debug << "add elem in cache\n";
-                if (pcache.cache_.size() == cache_size)
-                {
-                    pcache.hash_.erase(out->key_);
-                    pcache.cache_.erase(out);
-                }
-                pcache.cache_.emplace_front(cache_part::func((*slow_get_page)(hit), hit, next_using));
-                pcache.hash_.emplace(hit, pcache.cache_.begin());
-                out = pcache.cache_.begin();
-                for (auto i = pcache.cache_.begin(); i != pcache.cache_.end(); ++i)
-                {
-                    if (out->next_using < i->next_using)
-                        out = i;
-                }
-
-            }
-        }
-        debug << '\n';
-    }
-
-    return value;
-  }
-
-} //namespace cache
-
+}
--- PC/src/perfect_cache.cpp
+++ PC/src/perfect_cache.cpp
@@ -1,25 +1,33 @@
 #include <cassert>
 #include <iostream>
-#include <vector>
-#include <string>
 
-#include "perfect_cache.hpp"
+#include "PC.hh"
 
 // slow get page imitation
 int slow_get_page_int(int key) { return key; }
 
 int main() {
+    int hits = 0;
     int n;
     size_t m;
 
     std::cin >> m >> n;
     assert(std::cin.good());
-    int* lookup = new int[n]{};
+    cache::PC_cache<int> c(m);
+
+    int* hits_data = new int[n];
     for (int i = 0; i < n; ++i)
     {
-        std::cin >> lookup[i];
-        assert (std::cin.good());
+        std::cin >> hits_data[i];
+        assert(std::cin.good());
     }
 
-    std::cout << cache::lookup_update_perfect(lookup, n ,slow_get_page_int, m) << std::endl;
+    c.make_lookup(hits_data, n);
+    for (int i = 0; i < n; ++i)
+    {
+        if (c.lookup_update(hits_data[i], slow_get_page_int))
+            hits += 1;
+    }
+
+  std::cout << hits << std::endl;
 }
